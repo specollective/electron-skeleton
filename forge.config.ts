a8afdef03ae2ffdaa1b03ed84c4fe9488f3d5dd7
Joe Torreggiani <joetorreggiani@gmail.com>
2023-02-05 21:26:23 -0800

    Initial commit.

---
+++ forge.config.ts
@@ -0,0 +1,46 @@
+import type { ForgeConfig } from '@electron-forge/shared-types';
+import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerZIP } from '@electron-forge/maker-zip';
+import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerRpm } from '@electron-forge/maker-rpm';
+import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+
+import { mainConfig } from './webpack.main.config';
+import { rendererConfig } from './webpack.renderer.config';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
+
+const config: ForgeConfig = {
+  packagerConfig: {
+    // osxSign: {},
+    // osxNotarize: {
+    //   tool: 'notarytool',
+    //   appleId: process.env["APPLE_ID"],
+    //   appleIdPassword: process.env["APPLE_ID_PASS"],
+    //   teamId: process.env["APPLE_TEAM_ID"],
+    // },
+  },
+  rebuildConfig: {},
+  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  plugins: [
+    new WebpackPlugin({
+      mainConfig,
+      renderer: {
+        config: rendererConfig,
+        entryPoints: [
+          {
+            html: './src/index.html',
+            js: './src/renderer.ts',
+            name: 'main_window',
+            preload: {
+              js: './src/preload.ts',
+            },
+          },
+        ],
+      },
+    }),
+  ],
+};
+
+export default config;
